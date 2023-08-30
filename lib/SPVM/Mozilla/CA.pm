cd342eb2b695f1a5e6cee756037326ff95144434
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-30 09:11:48 +0900

    SSL_ca is implemented.

--- lib/SPVM/Mozilla/CA.pm
+++ lib/SPVM/Mozilla/CA.pm
@@ -6,32 +6,31 @@ our $VERSION = "0.001";
 
 =head1 Name
 
-SPVM::Mozilla::CA - Short Description
+SPVM::Mozilla::CA - Mozilla's CA cert bundle in PEM format
 
 =head1 Description
 
-The Mozilla::CA class of L<SPVM> has methods for someting.
+The Mozilla::CA class of L<SPVM> has methods to get Mozilla's CA cert bundle in PEM format.
 
 =head1 Usage
 
   use Mozilla::CA;
-
-=head1 Fields
-
-
+  
+  my $ssl_ca = Mozilla::CA->SSL_ca;
 
 =head1 Class Methods
 
+  static method SSL_ca : string ();
 
+Returns the content of the Mozilla's CA cert bundle PEM file.
 
-=head1 Instance Methods
+This is the same file content returned by L<Perl's Mozilla::CA::SSL_ca_file|Mozilla::CA/"SSL_ca_file">.
 
-
-
+The content is synced periodically.
 
 =head1 Repository
 
-
+L<SPVM::Mozilla::CA - Github|https://github.com/yuki-kimoto/SPVM-Mozilla-CA>
 
 =head1 Author
 
